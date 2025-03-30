6956ee78deb7a12799d1069f9eb02accca18d7c8
0xtomv <0xtomv@gmail.com>
2025-03-30 18:54:07 -0400

    Add endpoints

--- src/helius/helius_api.py
+++ src/helius/helius_api.py
@@ -1,11 +1,8 @@
-from helius.rpc20 import RpcAPI
-from helius.transactions import TransactionsAPI
+from helius.api_versions.rpc20 import RpcAPI
+from helius.api_versions.v0 import APIv0
 
 
-class HeliusAPI(RpcAPI, TransactionsAPI):
-    def __init__(self, api_key: str):
-        RpcAPI.__init__(self, api_key=api_key)
-        TransactionsAPI.__init__(self, api_key=api_key)
-
-    def test_function(self):
-        print("here")
+class HeliusAPI(RpcAPI, APIv0):
+    def __init__(self, api_key: str, request_prefix: str = "RPC20-"):
+        RpcAPI.__init__(self, api_key=api_key, request_prefix=request_prefix)
+        APIv0.__init__(self, api_key=api_key)
